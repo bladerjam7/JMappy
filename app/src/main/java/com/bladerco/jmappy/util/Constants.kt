a82e28c85b1b552ddfb4dd2b8ad9db12642bb2ca
John Le <johnhle7@gmail.com>
2021-02-04 14:08:53 -0500

    Removed Api Key

--- app/src/main/java/com/bladerco/jmappy/util/Constants.kt
+++ app/src/main/java/com/bladerco/jmappy/util/Constants.kt
@@ -5,14 +5,14 @@ class Constants{
     // https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=AIzaSyDh8h-16Q-uDkppVJsfPFjqS-U2OxSyfi8&photo_reference=ATtYBwJnVu_ObYPRi74zvUSPs-3YgmyUru1uhYfrPYHKr0LTnObVstfJyoOncrJvTnJDq9EYhDllfMVnsEIHR3_9DJEriqo_F2_nFEusbaRIqiDOgzkMeQu-wZNAyriMMt8p100xg3kLhgzpk544ur9hAcwdUs7-xE4UMtj8NVlKEDb0mLSH
 
     companion object{
-        const val IMAGE_URL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=AIzaSyDh8h-16Q-uDkppVJsfPFjqS-U2OxSyfi8&photoreference="
+        const val IMAGE_URL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=YOUR KEY HERE&photoreference="
 
         const val BASE_URL = "https://maps.googleapis.com/"
 
         // PATHS
         const val PLACE_PATH= "/maps/api/place/nearbysearch/json"
         const val ADDRESS_PATH = "/maps/api/geocode/json"
-        const val PHOTO_PATH = "/maps/api/place/photo?maxwidth=400&key=AIzaSyDh8h-16Q-uDkppVJsfPFjqS-U2OxSyfi8&"
+        const val PHOTO_PATH = "/maps/api/place/photo?maxwidth=400&key=YOUR KEY HERE"
 
         // QUERY HEADERS
         const val LOCATION= "location"
@@ -24,7 +24,7 @@ class Constants{
         const val PHOTO_REF = "photo_reference"
 
         // API KEY
-        const val MY_API_KEY = "AIzaSyAjT1auAsVYFR3fa2cDUui9lcb2LJ4rmYM"
+        const val MY_API_KEY = "YOUR KEY HERE"
 
         // Request codes
         const val LOCATION_REQUEST_CODE = 70
